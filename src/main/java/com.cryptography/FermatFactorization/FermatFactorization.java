2bab0d8c8255f67d9515eebb7b12d29771888046
Raphael G <raphaelgal@gmail.com>
2017-10-15 17:34:10 -0400

    Fermat factorization with BigInteger

--- src/main/java/com.cryptography/FermatFactorization/FermatFactorization.java
+++ src/main/java/com.cryptography/FermatFactorization/FermatFactorization.java
@@ -13,7 +13,7 @@ public class FermatFactorization implements Factorizer {
         long b2 = (a * a) - n;
         long b = (long) Math.sqrt(b2);
         long aMinusB = a - b;
-        long bound = 30;
+        long bound = 36;
         long prevAMinusB = a - b + bound;
 
         while (((long) Math.pow(Math.sqrt(b2), 2)) != b2 && (prevAMinusB - aMinusB) < bound) {
@@ -43,11 +43,62 @@ public class FermatFactorization implements Factorizer {
         }
 
         return result;
-
     }
 
     @Override
-    public BigInteger factorBigInteger(BigInteger bigIntegerToFactor) {
-        return null;
+    public BigInteger factorBigInteger(BigInteger n) {
+        BigInteger result = null;
+        BigInteger a = bigIntegerSqrt(n);
+        BigInteger b2 = a.multiply(a).subtract(n);
+        BigInteger b = bigIntegerSqrt(b2);
+        BigInteger aMinusB = a.subtract(b);
+        BigInteger bound = new BigInteger("30");
+        BigInteger prevAMinusB = aMinusB.add(bound);
+
+        while (!bigIntegerSqrt(b2).pow(2).equals(b2) && prevAMinusB.subtract(aMinusB).compareTo(bound) > -1) {
+            a = a.add(BigInteger.ONE);
+            b2 = a.multiply(a).subtract(n);
+            b = bigIntegerSqrt(b2);
+            prevAMinusB = aMinusB;
+            aMinusB = a.subtract(b);
+
+            if (bigIntegerSqrt(b2).pow(2).equals(b2)) {
+                result = aMinusB;
+            } else {
+                boolean finished = false;
+                BigInteger bigTwo = new BigInteger("2");
+                BigInteger p = aMinusB.add(bigTwo);
+                if (p.remainder(bigTwo).intValue() == 0) {
+                    p = p.add(BigInteger.ONE);
+                }
+                while (!finished) {
+                    p = p.subtract(bigTwo);
+                    if (n.remainder(p).equals(BigInteger.ZERO)) {
+                        finished = true;
+                    }
+                }
+                result = p;
+            }
+        }
+
+        return result;
+    }
+
+    private static BigInteger bigIntegerSqrt(BigInteger n) {
+        BigInteger r = BigInteger.ZERO;
+        BigInteger m = r.setBit(2 * n.bitLength());
+        BigInteger nr;
+
+        do {
+            nr = r.add(m);
+            if (nr.compareTo(n) != 1) {
+                n = n.subtract(nr);
+                r = nr.add(m);
+            }
+            r = r.shiftRight(1);
+            m = m.shiftRight(2);
+        } while (m.bitCount() != 0);
+
+        return r;
     }
 }
--- test/java/com.cryptography/CryptographyTests.java
+++ test/java/com.cryptography/CryptographyTests.java
@@ -20,9 +20,15 @@ class CryptographyTests {
     }
 
     @Test
-    void FermatFactorizatioTest() {
+    void FermatFactorizationTest() {
         assertThat(new FermatFactorization().factorLong(143)).isIn(13L, 11L);
         assertThat(new FermatFactorization().factorLong(988027)).isIn(991L, 997L);
 
+    }
+
+    @Test
+    void FermatFactorizationBigIntegerTest() {
+        assertThat(new FermatFactorization().factorBigInteger(new BigInteger("143"))).isIn(new BigInteger("13"), new BigInteger("11"));
+        assertThat(new FermatFactorization().factorBigInteger(new BigInteger("988027"))).isIn(new BigInteger("991"), new BigInteger("997"));
     }
 }
